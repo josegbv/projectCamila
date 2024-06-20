4a10939ae42b03281c9ab485c0b204b02cc073c3
jbrito <jbrito@chattigo.com>
2024-06-20 19:30:02 +0200

    all project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'proyectoCamila';
+  
+  estados = ["Molesto","Alegre",'Cansado',"Triste", 'Selecciona un estado de animo']
+  estadoSeleccionado:any = 'Selecciona un estado de animo';
+
+  colores:any = {
+    'Molesto': "red",
+    'Alegre': 'yellow',
+    'Cansado':  "gray ",
+    'Triste' : 'blue',
+    'Selecciona un estado de animo': 'white'
+  }
+
+  cambiarColor(){
+    document.body.style.backgroundColor = this.colores[this.estadoSeleccionado]
+  }
 }
